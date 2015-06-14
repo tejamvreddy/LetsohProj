a4b9627bf2b962b5be5e76b51c11f5f472935548
tejamvreddy <tejamvreddy@gmail.com>
2015-06-15 00:25:17 +0530

    Commit and push by tejamvreddy

--- src/com/letsoh/controller/LetsohHomeController.java
+++ src/com/letsoh/controller/LetsohHomeController.java
@@ -6,18 +6,38 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
+import com.letsoh.service.LetsohUserService;
+
 public class LetsohHomeController extends AbstractController{
+	
+	private LetsohUserService letsohUserService;
 	
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-
+		
+		System.out.println("Inside handleRequestInternal :: LetsohHomeController");
+		try {
+			letsohUserService.getAllUsers();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		ModelAndView model = new ModelAndView("letsohHome");
-		model.addObject("msg", "Hi there !!!");
+		model.addObject("msg", "Wiht Git!!!");
 
 		return model;
 	}
 
+	public LetsohUserService getLetsohUserService() {
+		return letsohUserService;
+	}
+
+	public void setLetsohUserService(LetsohUserService letsohUserService) {
+		this.letsohUserService = letsohUserService;
+	}
+
+	
 	
 
 }
--- src/com/letsoh/controller/LetsohLoginController.java
+++ src/com/letsoh/controller/LetsohLoginController.java
@@ -6,16 +6,35 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
+import com.letsoh.service.LetsohUserService;
+import com.letsoh.service.impl.LetsohUserServiceImpl;
+
 public class LetsohLoginController extends AbstractController{
+	
+	private LetsohUserService letsohUserService;
 	
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-
+		System.out.println("Inside handleRequestInternal :: LetsohLoginController");
+		try {
+			letsohUserService.getAllUsers();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		ModelAndView model = new ModelAndView("examples/login");
-		model.addObject("msg", "Login there !!!");
+		model.addObject("msg", "Login there Git !!!");
 
 		return model;
 	}
 
+	
+	public LetsohUserService getLetsohUserService() {
+		return letsohUserService;
+	}
+
+	public void setLetsohUserService(LetsohUserService letsohUserService) {
+		this.letsohUserService = letsohUserService;
+	}
 }
