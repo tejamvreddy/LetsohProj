9494846f8cf7d469cb9a0ce8d6031e99f2b389a4
tejamvreddy <tejamvreddy@gmail.com>
2015-06-21 16:48:08 +0530

    annotations spring change

--- src/com/letsoh/controller/LetsohHomeController.java
+++ src/com/letsoh/controller/LetsohHomeController.java
@@ -4,17 +4,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 import com.letsoh.service.LetsohUserService;
 
-public class LetsohHomeController extends AbstractController{
-	
+@Controller
+
+public class LetsohHomeController {
+	@Autowired
 	private LetsohUserService letsohUserService;
 	private static final Logger logger = Logger.getLogger(LetsohHomeController.class);
 	
-	@Override
+	/*@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		
@@ -29,8 +35,14 @@ public class LetsohHomeController extends AbstractController{
 		model.addObject("msg", "Wiht Git!!!");
 
 		return model;
-	}
+	}*/
 
+	@RequestMapping("/letsohHome.htm")
+	public ModelAndView testMethod(){
+		System.out.println("Inside testMethod -> Request Mappng Success");
+		return new ModelAndView("letsohHome");
+	}
+	
 	public LetsohUserService getLetsohUserService() {
 		return letsohUserService;
 	}
